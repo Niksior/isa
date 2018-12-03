039a4463f716b7318ea3e17a2eff24690d57bcfb
Niksior <heroes.VS.misio@gmail.com>
2018-12-03 19:21:58 +0100

    code changed for pc

--- cv_test.py
+++ cv_test.py
@@ -4,12 +4,8 @@
 import time
 from imutils.video import VideoStream
 import serial
-# from picamera.array import PiRGBArray
-# from picamera import PiCamera
 import numpy as np
 import cv2
-# import numpy as np
-# import typing
 
 
 def translate(value, oldMin, oldMax, newMin=-100, newMax=100):
@@ -21,24 +17,16 @@ def translate(value, oldMin, oldMax, newMin=-100, newMax=100):
 
     return int(NewValue)
 
-usesPiCamera = True
+usesPiCamera = False
+##### usesPiCamera = True
 
-# camera = PiCamera()
-# camera.framerate = 60
 cameraResolution = (640, 480)
-# camera.resolution = cameraResolution
-
-# # camera.awb_mode = 'tungsten'
-# camera.vflip = camera.hflip = True
-# camera.video_stabilization = True
-# rawCapture = PiRGBArray(camera, size=cameraResolution)
 
 # initialize the video stream and allow the cammera sensor to warmup
 vs = VideoStream(usePiCamera=usesPiCamera, resolution=cameraResolution, framerate=60).start()
 time.sleep(2.0)
 
 
-# cap = cv2.VideoCapture(0)
 blueLower = (0, 100, 50)
 blueUpper = (100, 255, 255)
 colorTolerance = 10
@@ -47,66 +35,48 @@ roiSize = (6, 6) # roi size on the scaled down image (converted to HSV)
 
 
 # initialize serial communication
-ser = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=0.05)
+###### ser = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=0.05)
 
 while True:
 # for cameraFrame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     loopStart = time.time()
     if not paused:
-        # ret, frame = cap.read()
-        # frame = cv2.flip(frame, flipCode=-1)
         frame = vs.read()
-        
         height, width = frame.shape[0:2]
         scaleFactor = 4
         newWidth, newHeight = width//scaleFactor, height//scaleFactor
-
-
         resizedColor = cv2.resize(frame, (newWidth, newHeight), interpolation=cv2.INTER_CUBIC)
         resizedColor_blurred = cv2.GaussianBlur(resizedColor, (5, 5), 0)
-
-        # resizedHSV = cv2.cvtColor(resizedColor, cv2.COLOR_BGR2HSV)
         resizedHSV = cv2.cvtColor(resizedColor_blurred, cv2.COLOR_BGR2HSV)
-
         roi = resizedHSV[newHeight//2 - roiSize[0]//2 : newHeight //2 + roiSize[0]//2, newWidth//2 - roiSize[1]//2 : newWidth//2 + roiSize[1]//2, :]
-        # roi = resizedHSV[10*newHeight//20 : 12*newHeight//20, 10*newWidth//20 : 12*newWidth // 20, :]
-        
         blueLowerWithTolerance = (blueLower[0] - colorTolerance,) + blueLower[1:]
         blueUpperWithTolerance = (blueUpper[0] + colorTolerance,) + blueUpper[1:]
-
         mask = cv2.inRange(resizedHSV, blueLowerWithTolerance, blueUpperWithTolerance)
         cv2.erode(mask, None, iterations=5)
         cv2.dilate(mask, None, iterations=5)
-
         (_,contours, hierarchy) = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
         boundingBoxes = []
         biggestObject_BoundingBox = None
         biggestObjectMiddle = None
         if contours:
             largestContour = max(contours, key=cv2.contourArea)
             biggestObject_BoundingBox = cv2.boundingRect(largestContour)
-            
             for i, contour in enumerate(contours):
                 area = cv2.contourArea(contour)
                 if area > ((newWidth * newHeight)/256):
                     x,y,w,h = cv2.boundingRect(contour)
                     boundingBoxes.append((x,y,w,h))
-                    # print("Object {}: ({},{}); {}x{}; area: {}".format(i, x,y,w,h, area))
         else:
             pass
-
         upscaledColor = cv2.resize(resizedColor, (width, height), interpolation=cv2.INTER_NEAREST)
         # draw ROI on upscaled image
         xROI, yROI = width//2 - roiSize[1]//2 * scaleFactor, height//2 - roiSize[0]//2 * scaleFactor
         cv2.rectangle(upscaledColor, (xROI, yROI), (xROI + roiSize[0]*scaleFactor, yROI + roiSize[1]*scaleFactor), (0, 0, 0), thickness=3)
-
         for boundingBox in boundingBoxes:
             x,y,w,h = boundingBox
             cv2.rectangle(resizedColor, (x, y), (x+w, y+h), (255, 255, 0), thickness=1)
             cv2.rectangle(upscaledColor, (x*scaleFactor, y*scaleFactor),
                         ((x+w)*scaleFactor, (y+h)*scaleFactor), (255, 255, 0), thickness=2)
-        
         if biggestObject_BoundingBox:
             x, y, w, h = biggestObject_BoundingBox
             biggestObjectMiddle = ((x+ w//2)*scaleFactor, (y + h//2)*scaleFactor)
@@ -116,31 +86,18 @@ while True:
             cv2.circle(upscaledColor, biggestObjectMiddle, 2, (255, 0, 0), thickness=2)
             screenMiddle = width//2, height//2
             distanceVector = tuple(map(lambda x, y: x - y, biggestObjectMiddle, screenMiddle))
-            # print("Vector: {}".format(distanceVector))
             scaled = (translate(distanceVector[0], -width//2, width//2), translate(distanceVector[1], -height//2, height//2) )
-            # print("Vector scaled: {}".format(scaled))
             cv2.line(upscaledColor, screenMiddle, biggestObjectMiddle, (0, 0, 255))
             packet = '<packet, {}, {}>'.format(scaled[0], scaled[1])
             packetBytes = bytes(packet, 'utf-8')
-            # yaw = 'y {}\n'.format(scaled[0])
-            # b_yaw = bytes(yaw, 'utf-8') # or 'ascii'
-            # pitch = 'p {}\n'.format(scaled[1])
-            # b_pitch = bytes(pitch, 'utf-8') # or 'ascii'
-            ser.write(packetBytes)
-            ser.read_all()
-            # ser.write(b_yaw)
-            # print(ser.read_all())
-            # ser.write(b_pitch)
-            # print(ser.read_all())
-            
-
+            ########ser.write(packetBytes)
+            ########ser.read_all()
         cv2.imshow("video", upscaledColor)
         cv2.imshow("roi", roi)
         cv2.imshow("mask", mask)
-
         modTolerances = False
 
-    # handle keys 
+    # handle keys
     key = cv2.waitKey(1) & 0xFF
     if key == ord('q'):
         break
@@ -170,14 +127,13 @@ while True:
         print("New color range: {}".format(colorTolerance))
     elif key == ord('p'):
         paused = not paused
-    elif key == ord('d'):
+    # elif key == ord('d'):
         # pause/unpause arduino camera movement
-        ser.write(bytes('d', 'utf-8'))
-    
-    # rawCapture.truncate(0)
-    loopEnd= time.time()
+        ########ser.write(bytes('d', 'utf-8'))
+
+    loopEnd = time.time()
     print("loop execution took {:3.2f}ms".format((loopEnd - loopStart)*1000))
-    
+
 # cleanup
 cv2.destroyAllWindows()
 vs.stop()
